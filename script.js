6647479c53cd32e01a6abb7802e295120426341e
ALeo75C <za.iz.pr@yandex.kz>
2019-03-26 00:14:43 +0300

    fin

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+let stop = {}
+
+document.addEventListener('DOMContentLoaded', function(){
+  createfigurs()
+  stop.chanel = Math.floor(Math.random() * 3)
+  stop.color = sumRandomParametrs(225)
+  stop.butColor = generateColor()
+  document.getElementById('start').setAttribute('style', 'border: 3px solid rgb(' + stop.butColor + '); background-color: rgb(' + stop.butColor + ');')
+})
+
 function createfigurs() {
   let number = readText('number')
   let speed1 = readText('speed1')
-  let speed2 = readText('speed2')
   let i = 0
   while (i < number) {
     let width = sumRandomParametrs(15)
@@ -10,30 +19,37 @@ function createfigurs() {
     let height = sumRandomParametrs(5)
     let top = cheackOffset(height)
 
-    speed1 = sumRandomParametrs(speed1) / 10
+    speed1 = 0.5 + sumRandomParametrs(speed1) / 10
 
     let newDiv = document.createElement('div')
-    document.getElementById('table').appendChild(newDiv).setAttribute('style', 'background-color:red; top: ' + top + 'vh; left: ' + left + 'vw; width: ' + width + 'vw; height: ' + height + 'vh; animation: spin '+ speed1 + 's linear infinite; background-color: rgb(' + generateColor() + '); transotion: all' + speed2 + 'ms ease')
+    document.getElementById('table').appendChild(newDiv).setAttribute('style', 'background-color:red; top: ' + top + 'vh; left: ' + left + 'vw; width: ' + width + 'vw; height: ' + height + 'vh; animation: spin '+ speed1 + 's linear infinite; background-color: rgb(' + generateColor() + ');')
     i += 1
   }
-  document.onmousemove = function(event) {
-    let element = document.getElementsByClassName('one')
-    element.style = 'top: ' + event.pageY + 'px; left: ' + event.pageX + 'px;'
+  let mess = document.getElementById('text').value
+  let el = document.getElementById('h1')
+  if (typeof el.textContent !== "undefined") {
+    el.textContent = mess
+  } else {
+    el.innerText = mess
   }
+  changeColorH1()
 }
 
-function clean() {
-  // tablediv = document.getElementById('table').cloneNode(true)
-  // tablediv.removeChild('div')
-  document.body.removeChild('div')
-}
+// function clean() {
+//   let tablediv = document.getElementById('table').cloneNode(true)
+//   // tablediv.removeChild('div')
+//   let elem = document.body.getElementsByTagName('div')
+//   elem = Array.from(elem)
+//   console.log(elem)
+//   tablediv.removeChild(document.getElementsByTagName('div'))
+// }
 
 function sumRandomParametrs(maxValue) {
   let a = Math.floor(Math.random() * 10)
   return Math.floor(Math.random() * maxValue) + a
 }
 function cheackOffset(a) {
-  let b = sumRandomParametrs(80)
+  let b = Math.floor(Math.random() * 100) - 5
   if (a + b > 120) {
     cheackOffset(a, b)
   } else {
@@ -41,14 +57,16 @@ function cheackOffset(a) {
   }
 }
 function generateColor() {
-  let r = sumRandomParametrs(250)
-  let g = sumRandomParametrs(250)
-  let b = sumRandomParametrs(250)
-  let color = [r, g, b]
-
-  const random = Math.floor(Math.random() * color.length)
-  color[random] = 0
-
+  let color = []
+  let chanel
+  for (i = 0; i < 3; i++) {
+    if (i === stop.chanel) {
+      chanel = stop.color
+    } else {
+      chanel = sumRandomParametrs(225)
+    }
+    color[i] = chanel
+  }
   return color.join()
 }
 
@@ -56,4 +74,15 @@ function readText(a) {
   let text = document.getElementById(a).value
   text = Number.parseInt(text)
   return text
+}
+
+function changeColorH1() {
+  let el = document.getElementById('color')
+  if ( el.selectedIndex != -1) {
+    if ( el.selectedIndex === 0 ) {
+      document.getElementById('h1').setAttribute('style', 'color: black')
+    } else {
+      document.getElementById('h1').setAttribute('style', 'color: white')
+    }
+  }
 }
